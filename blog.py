950678655c2bf08c41fc0208277462bad27dcef5
todehan <todehan@gmail.com>
2019-02-20 17:07:14 +0300

    basic routes

--- blog.py
+++ blog.py
@@ -2,6 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "Index Page"
+
+@app.route("/about")
+def about():
+    return "About Page"    
+
+@app.route("/contact")
+def contact():
+    return "Contact Page"
+
 if __name__ == "__main__":
     app.run(debug=True)
     
