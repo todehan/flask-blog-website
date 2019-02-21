e7fdf10e09f3a522d57183ca37a9abcc84b3ef4e
todehan <todehan@gmail.com>
2019-02-21 15:09:26 +0300

    required modules added

--- blog.py
+++ blog.py
@@ -1,4 +1,7 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,url_for,session,logging,request
+from flask_mysqldb import MySQL
+from wtforms import Form,StringField,TextAreaField,PasswordField,validators
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
@@ -14,9 +17,9 @@ def about():
 def contact():
     return render_template("contact.html")
 
-@app.route("/articles")
-def articles():
-    return render_template("articles.html")
+@app.route("/article/<string:id>")
+def article(id):
+    return render_template("article.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
